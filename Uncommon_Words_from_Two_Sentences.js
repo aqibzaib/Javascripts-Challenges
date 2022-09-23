784e8d1dc0ff929266418224cfb09244871bd81c
Aqib <96622567+aqibzaib@users.noreply.github.com>
2022-09-23 23:55:13 +0500

    Update Uncommon_Words_from_Two_Sentences.js

--- Uncommon_Words_from_Two_Sentences.js
+++ Uncommon_Words_from_Two_Sentences.js
@@ -1,20 +1,47 @@
-var s1 = "this apple is sweet";
-var s2 = "this apple is sour";
-var arr1;
-var arr2;
-var new_arr;
+var s1 = "apple apple";;
+var s2 = "banana";
+var output=[];
+//converting string to array
+s1=s1.split(" ");
+s2=s2.split(" ");
+
+
+
 var uncommonFromSentences = function(s1, s2) {
     
-    arr1=s1.split(" ");
-    arr2=s2.split(" ");
-
-    for(var i=0; i<arr1.length; i++){
-        // console.log(arr1[i].equals(arr2[i]));
-        if(!(arr1[i]==arr2[i])){
-            new_arr=[`${arr1[i]},${arr2[i]}`]
+    let hashMapS1 = {};
+    let hashMapS2={};
+ 
+    for(let i=0; i<s1.length; i++){
+        // we check weather we have value in our hashmap already or not 
+        if(!hashMapS1[s1[i]]){
+            ///now inserting the value
+            hashMapS1[s1[i]]  =1;
+        }else{
+            //if it's already increase the key number
+            hashMapS1[s1[i]] +=1;
         }
-
     }
-    console.log(new_arr);
+    for(let i=0; i<s2.length; i++){
+        if(!hashMapS2[s2[i]]){
+            hashMapS2[s2[i]]= 1;
+        }else{
+            hashMapS2[s2[i]] += 1;
+        }
+    }
+    
+     for(key in hashMapS1){
+        if((hashMapS1[key]===1 && !hashMapS2[key])){
+            output.push(key);
+        }
+     }
+     for(key in hashMapS2){
+        if((hashMapS2[key] === 1 && ! hashMapS1[key])){
+            output.push(key);
+        }
+     }
+
+console.log(output)    
 };
 uncommonFromSentences(s1,s2);
+
